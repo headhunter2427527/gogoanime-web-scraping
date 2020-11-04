ed4600ccfdf2a80836b1af7d116c1a8015b877fb
Bhaskar Srivastava <bhaskar.srivastava2011@gmail.com>
2020-11-04 19:50:23 +0530

    Day - 2 Added get details of all animes.

---
+++ getDetails.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+import pickle as pk
+
+with open('list', 'rb') as f:
+    animes = pk.load(f)
+
+DOMAIN = "https://gogoanime.so"
+
+
+def getDetails(anime):
+    source = requests.get(DOMAIN+animes[anime]["pageLink"]).text
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    details = soup.find('div', class_='anime_info_body_bg')
+
+    p = details.find_all('p', class_='type')
+
+    for detail in p:
+        heading = detail.span.text.split(':')
+
+        if detail.a:
+            a = detail.find_all('a')
+            for link in a:
+                value = link.text.split(',')
+                if not value[0]:
+                    animes[anime][heading[0]].append(value[1].strip())
+                else:
+                    if(len(a)>1):
+                        animes[anime][heading[0]] = [value[0].strip()]
+                    else:
+                        animes[anime][heading[0]] = value[0].strip()
+        else:
+            try:
+                animes[anime][heading[0]] = detail.contents[1]
+            except Exception as e:
+                pass
+
+    episode = soup.find('div', class_='anime_video_body')
+
+    totalEp = episode.find('a', class_='active')
+    totalEpisodes = totalEp['ep_end']
+
+    animes[anime]["Total Episodes"] = totalEpisodes
+
+    print(animes[anime])
+
+getDetails('One Piece')
+
